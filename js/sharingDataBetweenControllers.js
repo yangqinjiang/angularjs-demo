2a2d5720f614bd27d4592fb78bbbc0e6670c36a7
yangqinjiang <15976543860@139.com>
2014-08-07 11:12:12 +0800

    add Filter

--- js/sharingDataBetweenControllers.js
+++ js/sharingDataBetweenControllers.js
@@ -2,6 +2,11 @@ var myApp = angular.module('myApp',[]);
 myApp.factory('Data',function  () {
 	return {message:"I'm data from a service"};
 });
+myApp.filter('reverse',function(Data){
+	return function(text){
+		return text.split('').reverse().join("") +'   '+Data.message;
+	}
+});
 function FirstCtrl($scope,Data){
 	$scope.data=Data;
 }
