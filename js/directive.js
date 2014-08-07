8009fd491d0dabbb88089843d731128167c67045
yangqinjiang <15976543860@139.com>
2014-08-07 12:30:07 +0800

    First directive 3

--- js/directive.js
+++ js/directive.js
@@ -1,7 +1,9 @@
 var app=angular.module('myApp',[]);
 app.directive('superman',function  () {
 	return {
-		restrict:"E",//元素
-		template:"<div>Here I am to save the day.</div>"
+		restrict:"A",//
+		link:function(){
+			alert('I am working');
+		}
 	}
 });
